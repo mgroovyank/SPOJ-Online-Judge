8e5880d6a37d218b7b66423ad9e83f4db10f3b7d
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2019-05-09 11:36:56 +0530

    Create GIRLSNBS - Girls and Boys.cpp

---
+++ GIRLSNBS - Girls and Boys.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+  int g,b;
+  cin>>g>>b;
+  while(g!=-1 && b!=-1){
+    int t,m,q,r;
+    t=min(b,g)+1;
+    m=max(b,g);
+    q=m/t;
+    r=m%t;
+    if(t==1){
+      cout<<m<<"\n";
+    }else{
+      if(r==0){
+        cout<<q<<"\n";
+      }else{
+        cout<<q+1<<"\n";
+      }
+    }
+    cin>>g>>b;
+  }
+  return 0;
+}
